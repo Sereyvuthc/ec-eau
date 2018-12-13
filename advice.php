656bc5ec32126a6d05b8da3e6140ebf94ea69a78
Sereyvuth CHUM <sereyvuth.chum@hetic.net>
2018-12-13 10:20:47 +0100

    Add files via upload

---
+++ advice.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <?php include "header.php" ?>
+  </head>
+  <body>
+
+  <?php include "topbar.php" ?>
+
+    <?php include "footer.php" ?>
+
+  <?php include "js-import.php" ?>
+
+  </body>
+</html>
---
+++ topbar.php
@@ -0,0 +1,17 @@
+<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+  <div class="container">
+    <a class="navbar-brand" href="index.php"><i class="flaticon-pharmacy"></i><span>Re</span>Medic</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="oi oi-menu"></span> Menu
+    </button>
+    <div class="collapse navbar-collapse" id="ftco-nav">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item"><a href="index.php" class="nav-link">Accueil</a></li>
+        <li class="nav-item"><a href="advice.php" class="nav-link">Conseils</a></li>
+        <li class="nav-item"><a href="stats.php" class="nav-link">Statistiques</a></li>
+        <li class="nav-item"><a href="contact.php" class="nav-link">Contact</a></li>
+        <li class="nav-item cta"><a href="test.php" class="nav-link"><span>Faire le Test</span></a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
